2c8425b4050f53563860511012a22a1e0faaaf71
Daniel Margulis <damargulis@wustl.edu>
2018-02-17 21:09:04 -0600

    create account new

--- src/auth.js
+++ src/auth.js
@@ -25,7 +25,7 @@ router.post('/login', (req, res) => {
 });
 
 router.post('/createAccount', (req, res) => {
-	req.db.collection('user').findOne({username: req.body.username})
+	req.db.collection('user').findOne({username: req.body.user.username})
 	.then((user) => {
 		if(user) {
 			return Promise.reject(new UserError('Username Taken'));
@@ -33,15 +33,31 @@ router.post('/createAccount', (req, res) => {
 			return Promise.resolve();
 		}
 	}).then(() => {
-        var hashedPassword = passwordHash.generate(req.body.password);
+        var hashedPassword = passwordHash.generate(req.body.user.password);
 		return req.db.collection('user').insertOne({
-			username: req.body.username,
+			username: req.body.user.username,
             password: hashedPassword,
+            firstName: req.body.user.firstName,
+            age: req.body.user.age,
+            gender: req.body.user.gender,
+            occupation: req.body.user.occupation,
+            school: req.body.user.school,
+            interestsGender: req.body.user.interestsGender,
+            interestsDistance: req.body.user.interestsDistance,
+            interestsAgeMin: req.body.user.interestsAgeMin,
+            interestsAgeMax: req.body.user.interestsAgeMax,
+            attending: [],
+            liked: [],
+            disliked: [],
+            photos: [],
 		});
+    }).then(() => {
+        return req.db.collection('user').findOne({username: req.body.user.username})
 	}).then((user) => {
 		res.send(JSON.stringify({
 			success: true,
-			userId: user.insertedId,
+			userId: user._id,
+            user: user
 		}));	
 	}).catch((error) => {
 		console.log(error);
